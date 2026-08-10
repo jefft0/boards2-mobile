e5d9213ca7e89e5c2eca1db7a4c6731230db642c
Jeff Thompson <jeff@thefirst.org>
2026-08-10 15:47:32 +0200

    fix: threadReplyAndRedirectToSign: Rename boad to board
    Signed-off-by: Jeff Thompson <jeff@thefirst.org>

--- mobile/redux/features/threadReplySlice.ts
+++ mobile/redux/features/threadReplySlice.ts
@@ -37,18 +37,18 @@ export const threadReplyAndRedirectToSign = createAppAsyncThunk<void, CreateRepl
   'threadReply/CreateReply',
   async (props, thunkAPI) => {
     try {
-      const boad = selectThreadBoard(thunkAPI.getState() as RootState)
+      const board = selectThreadBoard(thunkAPI.getState() as RootState)
       const thread = selectThreadToReply(thunkAPI.getState() as RootState)
       const callerAddressBech32 = selectAccount(thunkAPI.getState() as RootState)?.bech32 as string
 
-      if (!boad || !thread) throw new Error('No active board or thread')
+      if (!board || !thread) throw new Error('No active board or thread')
 
       const { replyBody, callbackPath } = props
 
       const fnc = 'CreateReply'
       const gasFee = '1000000ugnot'
       const gasWanted = BigInt(50000000)
-      const args: string[] = [String(boad.id), String(thread.id), '0', replyBody]
+      const args: string[] = [String(board.id), String(thread.id), '0', replyBody]
       const reason = 'Reply a message'
 
       await makeCallTx({ fnc, args, gasFee, gasWanted, callerAddressBech32, reason, callbackPath }, thunkAPI.extra.gnonative)
